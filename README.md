a9816bffbd4f59e8d810692e29ec5f29f4e809fe
Brostafa <mostafahefny2010@gmail.com>
2019-02-26 10:44:04 +0200

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Google Hash Code
+
+## Pizza Problem
+
+### Getting started
+
+#### Requirements: 
+
+- Install [nodejs](https://nodejs.org/en/) (any version will be fine)
+- Install nodemon
+  - `npm install -g nodemon`
+
+---
+
+#### Installation
+```bash
+git pull https://github.com/Brostafa/HashCodePizza.git
+cd HashCodePizza
+nodemon # start watching index.js for changes
+```
+
+- Read `pizza.pdf` (you will find it in the project files)
+- Add your solution in `index.js` (search for `@TODO:`)
+- Happy coding :tada:
+
+
+You can submit your solution to https://hashcodejudge.withgoogle.com
---
+++ index.js
@@ -0,0 +1,82 @@
+const {
+  performance,
+  PerformanceObserver
+} = require('perf_hooks')
+const fs = require('fs')
+
+const readFile = path => fs.readFileSync(path, 'utf8')
+
+const examplePizza = readFile('./a_example.in')
+const smallPizza = readFile('./b_small.in')
+const mediumPizza = readFile('./c_medium.in')
+const bigPizza = readFile('./d_big.in')
+
+/**
+ * Slice pizza so that each slice will have the 
+ * right amount of tomatoes and mushrooms
+ * 
+ * @example
+ * -> pizza(`3 5 1 6
+ * TTTTT
+ * TMMMT
+ * TTTTT`)
+ * 
+ * -> `3
+ * 0 0 2 1
+ * 0 2 2 2
+ * 0 3 2 4`
+ * 
+ * @param {String} pizza pizza
+ * @returns {String} pizzaSlices
+ */
+const slicer = pizza => {
+  const dataset = pizza.trim().split('\n')
+  const [
+    totalRows,
+    totalColumns,
+    minimumIngredientsPerSlice,
+    maximumCellsPerSlice
+  ] = dataset.shift().split(' ') // remove first line of the dataset
+
+
+  console.log(`Total rows:                       R = %d`, totalRows)
+  console.log(`Total columns:                    C = %d`, totalColumns)
+  console.log(`Minimum ingredients per slice:    L = %d`, minimumIngredientsPerSlice)
+  console.log(`Maximum cells per slice:          H = %d`, maximumCellsPerSlice)
+
+  // @TODO: Your algorthim here
+  console.log('Dataset:', dataset)
+  
+
+}
+
+// measure how many miliseconds (and microseconds) the script
+// takes to solve a problem
+const measureRuntime = func => {
+  const wrapped = performance.timerify(func)
+
+  const obs = new PerformanceObserver(list => {
+    const runtime = list.getEntries()[0].duration
+    
+    console.log('==========================================')
+    console.log('Total execution time: \u001b[31m%d ms\u001b[39m', runtime)
+    console.log('==========================================')
+
+    obs.disconnect()
+  })
+
+  obs.observe({ entryTypes: ['function'] })
+  
+  // A performance timeline entry will be created
+  wrapped()
+}
+
+// after solving this one, feel free to try with other pizzas 
+measureRuntime(() => slicer(examplePizza))
+// measureRuntime(() => slicer(smallPizza))
+// measureRuntime(() => slicer(mediumPizza))
+// measureRuntime(() => slicer(bigPizza))
+
+
+
+
